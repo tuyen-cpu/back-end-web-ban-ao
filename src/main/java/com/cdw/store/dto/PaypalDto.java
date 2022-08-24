dc01b7d7aef08657bab156ad8bd5f3ba2c37b46c
Tấn Tài <taitan.tran@dxc.com>
2022-08-24 10:58:25 +0700

    paypal v2

--- src/main/java/com/cdw/store/dto/PaypalDto.java
+++ src/main/java/com/cdw/store/dto/PaypalDto.java
@@ -1,2 +1,19 @@
-package com.cdw.store.dto;public class PaypalDto {
+package com.cdw.store.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PaypalDto {
+    private double price;
+    private String currency;
+    private String method;
+    private String intent;
+    private String description;
 }
